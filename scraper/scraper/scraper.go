cab5fe110f50911dcfe95b6493c7c61cf6b23050
tts2k <tranthaison2000@gmail.com>
2024-04-20 17:12:07 +0700

    feat(scraper): parse TOC

--- scraper/scraper/scraper.go
+++ scraper/scraper/scraper.go
@@ -6,23 +6,81 @@ import (
 	"strings"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 	"github.com/microcosm-cc/bluemonday"
 )
 
-type Article = struct {
+type TOCItem struct {
+	ID       string
+	Content  string
+	SubItems []TOCItem
+}
+
+type Article struct {
 	Title    string
 	Issued   time.Time
 	Modified time.Time
 	HTMLText string
 	Author   []string
+	TOC      []TOCItem
 }
 
 var Collector = colly.NewCollector()
 
 var timeLayout = "2006-01-02"
 var htmlTrimRegex = regexp.MustCompile(`[\t\r\n]+`)
+var tocContentRegex = regexp.MustCompile(`^[^a-zA-Z]+`)
 var sanitizer = bluemonday.UGCPolicy()
+
+func parseTOCRecur(root *goquery.Selection) []TOCItem {
+	var toc []TOCItem
+
+	root.Children().Each(func(_ int, sel *goquery.Selection) {
+		tagName := goquery.NodeName(sel)
+
+		if tagName != "li" {
+			return
+		}
+
+		sel.Children().Each(func(_ int, subSel *goquery.Selection) {
+			subTagName := goquery.NodeName(subSel)
+			if subTagName == "a" {
+				href, _ := subSel.Attr("href")
+
+				// Skip Academic Tools
+				if href == "#Aca" {
+					return
+				}
+
+				// Trim space and new lines
+				content := tocContentRegex.ReplaceAllString(strings.TrimSpace(subSel.Text()), "")
+				content = htmlTrimRegex.ReplaceAllString(content, " ")
+
+				toc = append(toc, TOCItem{
+					ID:      href[1:],
+					Content: content,
+				})
+
+				return
+			}
+
+			if subTagName == "ul" {
+				// Find sub list
+				currTocItem := &toc[len(toc)-1]
+				currTocItem.SubItems = parseTOCRecur(subSel)
+			}
+
+		})
+
+	})
+
+	return toc
+}
+
+func parseTOC(root *goquery.Selection) []TOCItem {
+	return parseTOCRecur(root.Find("ul").First())
+}
 
 func Single(url string) (Article, error) {
 	article := Article{}
@@ -53,6 +111,10 @@ func Single(url string) (Article, error) {
 		}
 
 		article.Modified = modifiedTime
+	})
+
+	Collector.OnHTML("div[id='toc']", func(e *colly.HTMLElement) {
+		article.TOC = parseTOC(e.DOM)
 	})
 
 	Collector.OnHTML("div[id='aueditable']", func(e *colly.HTMLElement) {
--- scraper/scraper/scraper_test.go
+++ scraper/scraper/scraper_test.go
@@ -1,12 +1,15 @@
 package scraper
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func newUnstartedTestServer() *httptest.Server {
@@ -100,11 +103,147 @@ func TestSingle(t *testing.T) {
 	}
 
 	if strings.Contains(article.HTMLText, `id="toc"`) {
-		t.Fatalf("TOC is not filtered from HTML Text")
+		t.Fatal("TOC is not filtered from HTML Text")
 	}
 
 	if strings.Contains(article.HTMLText, `id="academic-tools"`) {
-		t.Fatalf("Academic tools is not filtered from HTML Text")
+		t.Fatal("Academic tools is not filtered from HTML Text")
 	}
 
+}
+
+func compareTOCs(rootID string, t1 []TOCItem, t2 []TOCItem) error {
+	if len(t1) != len(t2) {
+		return fmt.Errorf(
+			"Sub items length on %s mismatch. Expected %d but got %d",
+			rootID,
+			len(t1),
+			len(t2),
+		)
+	}
+
+	for i := range t1 {
+		if t1[i].ID != t2[i].ID {
+			return fmt.Errorf(
+				"ID mismatch. Expected %s to equal %s.",
+				t1[i].ID,
+				t2[i].ID,
+			)
+		}
+
+		if t1[i].Content != t2[i].Content {
+			return fmt.Errorf(
+				"Content mismatch. Expected \"%s\" to equal \"%s\".",
+				t1[i].Content,
+				t2[i].Content,
+			)
+		}
+
+		if len(t1[i].SubItems) > 0 {
+			err := compareTOCs(t1[i].ID, t1[i].SubItems, t2[i].SubItems)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func TestParseToc(t *testing.T) {
+	file, _ := os.Open("./testdata/full_document.html")
+	gq, _ := goquery.NewDocumentFromReader(file)
+
+	testToc := []TOCItem{
+		{
+			ID:      "BiogSket",
+			Content: "Biographical Sketch",
+		},
+		{
+			ID:      "InteBack",
+			Content: "Intellectual Background",
+		},
+		{
+			ID:      "MajoWork",
+			Content: "Major Works",
+			SubItems: []TOCItem{
+				{
+					ID:      "HistMadnMedi",
+					Content: "Histories of Madness and Medicine",
+				},
+				{
+					ID:      "OrdeThin",
+					Content: "The Order of Things",
+					SubItems: []TOCItem{
+
+						{
+							ID:      "ClasRepr",
+							Content: "Classical Representation",
+						},
+						{
+							ID:      "KantCritClasRepr",
+							Content: "Kant’s Critique of Classical Representation",
+						},
+						{
+							ID:      "LangMan",
+							Content: "Language and “Man”",
+						},
+						{
+							ID:      "AnalFini",
+							Content: "The Analytic of Finitude",
+						},
+					},
+				},
+				{
+					ID:      "ArchGene",
+					Content: "From Archaeology to Genealogy",
+				},
+				{
+					ID:      "HistPris",
+					Content: "History of the Prison",
+				},
+				{
+					ID:      "HistModeSexu",
+					Content: "History of Modern Sexuality",
+				},
+				{
+					ID:      "SexAnciWorl",
+					Content: "Sex in the Ancient World",
+				},
+			},
+		},
+		{
+			ID:      "FoucAfteFouc",
+			Content: "Foucault after Foucault",
+		},
+		{
+			ID:      "Bib",
+			Content: "Bibliography",
+			SubItems: []TOCItem{
+				{
+					ID:      "PrimSour",
+					Content: "Primary Sources",
+				},
+				{
+					ID:      "SecoSour",
+					Content: "Secondary Sources",
+				},
+			},
+		},
+		{
+			ID:      "Oth",
+			Content: "Other Internet Resources",
+		},
+		{
+			ID:      "Rel",
+			Content: "Related Entries",
+		},
+	}
+
+	result := parseTOC(gq.Find("div[id='toc']").First())
+
+	err := compareTOCs("root", testToc, result)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
