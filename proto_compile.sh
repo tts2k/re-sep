312a1c3046c6f27f87e456e3667c6b4410ab8ca9
tts2k <tranthaison2000@gmail.com>
2024-07-03 20:28:29 +0700

    fix: regenerate proto and move proto file location

---
+++ proto_compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Content
+protoc --go_out=./server/content/internal/ --go_opt=paths=source_relative \
+ --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+ ./proto/*.proto
+
+# User
+protoc --go_out=./server/user/internal/ --go_opt=paths=source_relative \
+ --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+ ./proto/*.proto
+
+# Client
+cd ./client && protoc --plugin=$(npm root)/.bin/protoc-gen-ts_proto \
+ --ts_proto_out=./src/lib/proto \
+ --ts_proto_opt=outputServices=grpc-js \
+ --ts_proto_opt=esModuleInterop=true \
+ -I=src/lib/proto ./src/lib/proto/*.proto
