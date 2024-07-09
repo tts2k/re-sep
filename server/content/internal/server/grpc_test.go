b2ccc58b67120414adffef787ce0746983d751f7
tts2k <tranthaison2000@gmail.com>
2024-07-09 12:12:10 +0700

    feat(server): applying template

--- server/content/internal/server/grpc_test.go
+++ server/content/internal/server/grpc_test.go
@@ -6,10 +6,15 @@ import (
 	"time"
 
 	"re-sep-content/internal/database"
-	"re-sep-content/internal/proto"
+	pb "re-sep-content/internal/proto"
+
+	"google.golang.org/grpc"
 )
 
 type mockDatabaseService struct{}
+type mockAuthClient struct {
+	pb.AuthClient
+}
 
 func (mdb *mockDatabaseService) Health() map[string]string {
 	return nil
@@ -23,17 +28,40 @@ func (mdb *mockDatabaseService) GetArticle(_ string) *database.Article {
 		Modified:  time.Now().Format(time.RFC3339),
 		HTMLText:  "<div>test<div>",
 		Author:    `["Tester 1", "Tester 2"]`,
-		TOC:       `[{"id":"Item 1","content":"Item 1"},{"id":"Item 2","content":"Item 2","subItems":[{"id":"Item 2-1","content":"Item 2-1"},{"id":"Item 2-2","content":"Item 2-2"}]},{"id":"Bib","content":"Bibliography","subItems":[{"id":"PrimSour","content":"Primary Sources"},{"id":"SecoSour","content":"Secondary Sources"}]},{"id":"Oth","content":"Other Internet Resources"},{"id":"Rel","content":"Related Entries"}]`}
+		TOC:       `[{"id":"Item 1","content":"Item 1"},{"id":"Item 2","content":"Item 2","subItems":[{"id":"Item 2-1","content":"Item 2-1"},{"id":"Item 2-2","content":"Item 2-2"}]},{"id":"Bib","content":"Bibliography","subItems":[{"id":"PrimSour","content":"Primary Sources"},{"id":"SecoSour","content":"Secondary Sources"}]},{"id":"Oth","content":"Other Internet Resources"},{"id":"Rel","content":"Related Entries"}]`,
+	}
+}
+
+func (*mockAuthClient) Auth(ctx context.Context, in *pb.Empty, opts ...grpc.CallOption) (*pb.AuthResponse, error) {
+	return nil, nil
+}
+func (*mockAuthClient) UpdateUsername(ctx context.Context, in *pb.Username, opts ...grpc.CallOption) (*pb.User, error) {
+	return nil, nil
+}
+func (*mockAuthClient) UpdateUserConfig(ctx context.Context, in *pb.UserConfig, opts ...grpc.CallOption) (*pb.UserConfig, error) {
+	return nil, nil
+}
+func (*mockAuthClient) GetUserConfig(ctx context.Context, in *pb.Empty, opts ...grpc.CallOption) (*pb.UserConfig, error) {
+	return &pb.UserConfig{
+		Font:     "serif",
+		FontSize: 3,
+		Justify:  false,
+		Margin: &pb.Margin{
+			Left:  3,
+			Right: 3,
+		},
+	}, nil
 }
 
 func TestGetArticle(t *testing.T) {
 	mockContentServer := contentServer{
-		db: &mockDatabaseService{},
+		db:         &mockDatabaseService{},
+		authClient: &mockAuthClient{},
 	}
 
 	article, err := mockContentServer.GetArticle(
 		context.TODO(),
-		&proto.EntryName{
+		&pb.EntryName{
 			EntryName: "test",
 		},
 	)
